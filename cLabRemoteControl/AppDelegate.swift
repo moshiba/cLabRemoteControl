47329361ed1b86bfe6fa1ab1df4028ad5dd5bc6e
hsuantinglu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2018-05-17 21:12:32 +0800

    Initial Commit

---
+++ cLabRemoteControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cLabRemoteControl
+//
+//  Created by Tim Lu on 2018/5/17.
+//  Copyright © 2018 Demilab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ cLabRemoteControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cLabRemoteControl
+//
+//  Created by Tim Lu on 2018/5/17.
+//  Copyright © 2018 Demilab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
