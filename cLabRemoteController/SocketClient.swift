5a585e6d7f6567f017162347df4b0642386d1b94
Yoda <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2018-05-30 14:04:59 +0800

    brutal override

--- cLabRemoteController/SocketClient.swift
+++ cLabRemoteController/SocketClient.swift
@@ -16,72 +16,56 @@ class SocketClient : NSObject{
     let streamPort:  UInt32 = 8888
     let commandPort: UInt32 = 8889
     let maxReadLength:  Int = 16384
-
+    
     weak var imageDelegate: ImageDelegate?
-
+    
     var host: String!
     var imageInputStream: InputStream!
     var commandOutputStream: OutputStream!
-
+    
     override init() {
         print("on Main thread?", Thread.isMainThread)
         print("multi thread?", Thread.isMultiThreaded())
     }
-
+    
     func setConn(host: String) {
-        if imageInputStream != nil {
-            if (imageInputStream.streamStatus.rawValue != 6) {
-                imageInputStream.close()
-            }
-        }
-        if commandOutputStream != nil {
-            if (commandOutputStream.streamStatus.rawValue != 6) {
-                commandOutputStream.close()
-            }
-        }
-        
         self.host = host
         print("connection set for HOST:", self.host)
-
-        var imgReadStream: Unmanaged<CFReadStream>?
-        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault,
-                                           self.host as CFString,
-                                           self.streamPort,
-                                           &imgReadStream,
-                                           nil)
+        
+        // img
+        var imgReadStream:  Unmanaged<CFReadStream>?
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, self.host as CFString, self.streamPort, &imgReadStream, nil)
+        
+        // cmd
         var cmdWriteStream: Unmanaged<CFWriteStream>?
-        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault,
-                                           self.host as CFString,
-                                           self.commandPort,
-                                           nil,
-                                           &cmdWriteStream)
-
-        imageInputStream = imgReadStream!.takeRetainedValue()
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, self.host as CFString, self.commandPort, nil, &cmdWriteStream)
+        
+        // img
+        imageInputStream  = imgReadStream!.takeRetainedValue()
+        
+        // cmd
         commandOutputStream = cmdWriteStream!.takeRetainedValue()
-
-        // img stream delegation
+        
         imageInputStream.delegate = self
-
+        
         imageInputStream.schedule(in: .current, forMode: .commonModes)
         commandOutputStream.schedule(in: .current, forMode: .commonModes)
-
+        
         imageInputStream.open()
         commandOutputStream.open()
     }
-
+    
     func sendCommand(cmd: String) {
         // Cmd is made of tuple (RightWheelDutyCycle, LeftWheelDutyCycle)
-        
-        // Constructs command with keyword in front, seperated by commas
-        let command = "NCTUEEclass20htlu, \(cmd)".data(using: .ascii)!
+        let command = "NCTUEEclass20htlu, \(cmd)".data(using: .ascii)!  // Constructs command with keyword in front, seperated by commas
         _ = command.withUnsafeBytes{commandOutputStream.write($0, maxLength: command.count)}
     }
-
+    
     deinit {
         print("socket CLIENT destruct STARTS")
         imageInputStream.close()
         commandOutputStream.close()
-
+        
         imageInputStream.delegate = nil
         imageInputStream = nil
         commandOutputStream = nil
@@ -92,72 +76,57 @@ class SocketClient : NSObject{
 extension SocketClient: StreamDelegate {
     func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
         switch eventCode {
-            case Stream.Event.hasBytesAvailable:
-                readAvailableBytes(stream: aStream as! InputStream)
-                //print("new message received: HBA")
-                print("delegate: [MSG STARTS]")
-                break
-
-            case Stream.Event.endEncountered:
-                //print("new message received: EE")
-                print("delegate: [MSG ENDS]")
-                break
-
-            case Stream.Event.errorOccurred:
-                print("delegate: error occurred")
-                break
-
-            case Stream.Event.hasSpaceAvailable:
-                print("delegate: has space available")
-                break
-
-            case Stream.Event.openCompleted:
-                print("delegate: open completed")
-                break
-
-            default:
-                print("delegate: some other event..., eventCode \(eventCode)")
-                break
+        case Stream.Event.hasBytesAvailable:
+            readAvailableBytes(stream: aStream as! InputStream)
+            print("new message received: HBA")
+            
+        case Stream.Event.endEncountered:
+            print("new message received: EE")
+            
+        case Stream.Event.errorOccurred:
+            print("error occurred")
+            
+        case Stream.Event.hasSpaceAvailable:
+            print("has space available")
+            
+        default:
+            print("some other event..., eventCode \(eventCode)")
+            break
         }
     }
-
+    
     private func readAvailableBytes(stream: InputStream) {
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: self.maxReadLength)
-
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxReadLength)
+        
         while stream.hasBytesAvailable {
-            let numberOfBytesRead = imageInputStream.read(buffer, maxLength: self.maxReadLength)
-
+            let numberOfBytesRead = imageInputStream.read(buffer, maxLength: maxReadLength)
+            
             if numberOfBytesRead < 0 {
                 if let _ = stream.streamError {
-                    print("stream error: numberOfBytesRead < 0")
                     break
                 }
             }
-
-            // Construct the Message object
+            
+            //Construct the Message object
+            
             if let message = processedMessageString(buffer: buffer, length: numberOfBytesRead) {
-                // Notify interested parties
-                print("msg constructed:", message)
-
+                //Notify interested parties
                 imageDelegate?.receivedMessage(message: message)
-
-                print("notify others")
             }
         }
         //buffer.deallocate()
     }
-
+    
     private func processedMessageString(buffer: UnsafeMutablePointer<UInt8>, length: Int) -> String? {
         guard let stringArray = String(bytesNoCopy: buffer,
                                        length: length,
-                                       encoding: .ascii,
+                                       encoding: .utf8,
                                        freeWhenDone: true)?.components(separatedBy: " "),  // split ImgByteStr by
             let imageStr = stringArray.first else {
-                print("string array: nil")
                 return nil
         }
         print("rcve photo")
-        // FIXME: 
+        // FIXME:
         print("string array", stringArray, ", imageStr", imageStr)
         
         
